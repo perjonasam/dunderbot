12dc82663266f9650193109fc0adedd9b7c6e816
Jonas Alström Mortin <jonas.a.mortin@gmail.com>
2020-07-20 22:47:53 +0200

    Modularized run code for easier interface and to unblock dynamic data loading and train/test handling

---
+++ src/run.py
@@ -0,0 +1,32 @@
+import json
+import datetime as dt
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+import pandas as pd
+from src.env.dunderbot_env import DunderBotEnv
+
+def load():
+    df = pd.read_csv('./data/input/AAPL.csv')
+    return df
+
+def preprocess(*, df):
+    df = df.sort_values('Date')
+    # The algorithms require a vectorized environment to run
+    env = DummyVecEnv([lambda: DunderBotEnv(df)])
+    return env
+
+def train(*, env, total_timesteps=20000):
+    model = PPO2(MlpPolicy, env, verbose=1)
+    model.learn(total_timesteps=total_timesteps, log_interval=10)
+    return model
+
+def predict(*, env, model, total_timesteps=2000):
+    obs = env.reset()
+    for i in range(total_timesteps):
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        if i % 100 == 0:
+            env.render()
