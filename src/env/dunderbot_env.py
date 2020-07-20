dc3ca812c303987c64a40f132756156511b0c6c1
Jonas Alström Mortin <jonas.a.mortin@gmail.com>
2020-07-20 22:34:49 +0200

    Added more advanced actions: ratios of possible amount

--- src/env/dunderbot_env.py
+++ src/env/dunderbot_env.py
@@ -26,13 +26,15 @@ class DunderBotEnv(gym.Env):
         self.df = df
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
 
-        # For now: only buy or sell fized amounts, or hold
-        self.action_space = spaces.Discrete(3)
+        # n_value_bins of different ratio
+        self.action_n_bins = int(config.action_strategy.n_value_bins)
+        self.action_space = spaces.Discrete(2 * self.action_n_bins + 1)
 
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
             low=0, high=1, shape=(6, 6), dtype=np.float16)
 
+    
 
     def _next_observation(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
@@ -62,20 +64,24 @@ class DunderBotEnv(gym.Env):
         return obs
 
     def _take_action(self, action):
+        """
+        There are n_value_bins (=self.action_n_bins) actions per buy and sell, marking a range of ratios of possible assets to buy and sell,
+        according to 1/(bin_value+1) since we want a buy/sell ratio of max 1/2. Hold uses only one action.
+        
+        First n_value_bins actions are buy, next n_value_bins sell, and lastly single hold.
+        """
+        # TODO: test impact of different orders of actions
+        
+        # TODO: consider setting this to closing price instead
         # Set the current price to a random price within the time step
         current_price = random.uniform(
             self.df.loc[self.current_step, "Open"], self.df.loc[self.current_step, "Close"])
 
-        action = action
-        amount = config.action.fixed_amount
-
-        if action == 0:  # Buy
-            # Buy fixed amount of asset
+        if action < self.action_n_bins:  # Buy
             total_possible = int(self.balance / current_price)
-            if total_possible >= amount:
-                shares_bought = amount
-            else:
-                shares_bought = 0
+            ratio = 1/(action + 2)  # +1 for 0 first of array, and +1 for 1/2 as max ratio
+            shares_bought = int(total_possible * ratio)
+           
             prev_cost = self.cost_basis * self.shares_held
             additional_cost = shares_bought * current_price
 
@@ -84,12 +90,9 @@ class DunderBotEnv(gym.Env):
                 prev_cost + additional_cost) / (self.shares_held + shares_bought)
             self.shares_held += shares_bought
 
-        elif action == 1:  # Sell
-            # Sell fixed amount of asset
-            if self.shares_held >= amount: 
-                shares_sold = amount
-            else:
-                shares_sold = 0
+        elif action >= self.action_n_bins and action < 2 * self.action_n_bins:  # Sell
+            ratio = 1/((action-self.action_n_bins) + 2)
+            shares_sold = int(self.shares_held * ratio)
 
             self.balance += shares_sold * current_price
             self.shares_held -= shares_sold
