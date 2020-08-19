Normalizing obs and rewards, save+load enabled, other small fixes

src/env/DunderBotEnv.py
@@ -40,7 +40,7 @@ class DunderBotEnv(gym.Env):
         # Observations are price and volume data the last data_n_timesteps, and portfolio features
         self.obs_array_length = self.data_n_timesteps*2 + int(config.n_nonprice_features)
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(1, self.obs_array_length), dtype=np.float16)
+            low=-np.inf, high=np.inf, shape=(1, self.obs_array_length), dtype=np.float16)
 
         # Set trade strategy with some constants
         # TODO: select best values here (now using default in RLTrader)
@@ -75,22 +75,23 @@ class DunderBotEnv(gym.Env):
         # Get the price+volume data points for the last data_n_indexsteps days and scale to between 0-1
         obs = np.array([
             self.df.loc[self.current_step - self.data_n_indexsteps: self.current_step
-                        , 'Close'].values / MAX_SHARE_PRICE])
+                        , 'Close'].values])
         obs = np.append(obs, [[
             self.df.loc[self.current_step - self.data_n_indexsteps: self.current_step
-                        , 'VolumeUSD'].values / MAX_NUM_ASSET,
+                        , 'VolumeBTC'].values,
         ]])
 
         # Non-price/volume features
         # NOTE: if changed, don't forget to set n_features in config
         obs = np.append(obs, [[
-            self.balance / MAX_ACCOUNT_BALANCE,
-            self.net_worths[-1] / MAX_ACCOUNT_BALANCE,
-            self.asset_held / MAX_NUM_ASSET,
+            self.balance,
+            self.net_worths[-1],
+            self.asset_held,
         ]])
 
-        assert np.logical_and(obs >= 0, obs <= 1).all(), f'Observation is ouside of range [0,1]'
-
+        #assert np.logical_and(obs >= 0, obs <= 1).all(), f'Observation is ouside of range [0,1]'
+        assert not np.isnan(np.sum(obs)), f'Observation contains nan'
+        assert not np.isinf(obs).any(), f'Observation contains inf'
         assert len(obs) == self.obs_array_length, \
             f'Actual obs array is {len(obs)} long, but specified to be {self.obs_array_length}'
 

src/env/render/TradingChartStatic.py
@@ -75,7 +75,7 @@ class TradingChartStatic:
 
     def _render_volume(self, times):
         self.volume_ax = self.price_ax.twinx()
-        volume = np.array(self.df['VolumeUSD'].loc[self.step_range])
+        volume = np.array(self.df['VolumeBTC'].loc[self.step_range])
 
         self.volume_ax.plot(mdates.date2num(times), volume,  color='blue')
         self.volume_ax.fill_between(mdates.date2num(times), volume, color='blue', alpha=0.5)

src/run.py
@@ -1,10 +1,13 @@
 import json
 import datetime as dt
+import os
 
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
 from stable_baselines import PPO2
+from stable_baselines.common.env_checker import check_env
 
+import numpy as np
 import pandas as pd
 import gym
 from src.env.DunderBotEnv import DunderBotEnv
@@ -15,29 +18,61 @@ config = get_config()
 def preprocess(*, df):
     # The algorithms require a vectorized environment to run
     env = DunderBotEnv(df=df, train_test='train')
-    #env = TrainTestWrapper(env, max_steps=100, train_test='train')
+    # check env is designed correctly
+    #check_env(env)
     env = DummyVecEnv([lambda: env])
+    env = VecNormalize(env, training=True, norm_obs=True, norm_reward=True, clip_obs=np.inf)
     return env
 
 
-def train(*, env, timesteps):
+def _save(*, env, model, save_dir):
+    print(f'Saving files to {save_dir}')
+    model.save(save_dir + "PPO2")
+    stats_path = os.path.join(save_dir, "vec_normalize.pkl")
+    env.save(stats_path)
+
+
+def _load(*, df, train_test, save_dir):
+    print(f'Loading files from {save_dir}')
+    stats_path = os.path.join(save_dir, "vec_normalize.pkl")
+    # Load the agent
+    model = PPO2.load(save_dir + "PPO2")
+    
+    env = DunderBotEnv(df=df, train_test=train_test)
+    # Load the saved statistics
+    env = DummyVecEnv([lambda: env])
+    env = VecNormalize.load(stats_path, env)
+    model.set_env(env)
+    print(f'Model connected with env')
+    return env, model
+
+
+def train(*, env, timesteps, save_dir="/tmp/"):
     model = PPO2(MlpPolicy, env, tensorboard_log=config.monitoring.tensorboard.folder, verbose=1)
     model.learn(total_timesteps=timesteps, log_interval=10)
+    # Save model and env
+    _save(env=env, model=model, save_dir=save_dir)
     return model
 
 
-def predict(*, df, model, timesteps, rendermode='human'):
-    # Same env as above, but with different train_test setting
-    env = DunderBotEnv(df=df, train_test='test')
-    env = DummyVecEnv([lambda: env])
-    obs = env.reset()
+def predict(*, df, timesteps, save_dir="/tmp/", rendermode='human'):
+    # Load model anf env stats from file
+    env, model = _load(df=df, train_test='test', save_dir=save_dir)
+    env.training = False
+    env.norm_reward = False
     
+    print(f'Predicting for {timesteps} timesteps')
+    obs = env.reset()
     done = False
+    obs_all=[]
     for i in range(timesteps):
         action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
+        obs, reward, done, info = env.step(action)
+        obs_all.append(obs)
         if done:
             print(f'Env done, loop {i+1}')
             break
     
     env.render(mode=rendermode)
+    return obs_all
+